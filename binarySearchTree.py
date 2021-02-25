abe2c7055a2a7bdfef6cdc9a3a9610460551efa9
Daniela Carvalho <daniela.czarref@gmail.com>
2021-02-25 20:51:51 -0300

    D - development of search and pre order functions for binary search tree

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -28,3 +28,33 @@ class Tree:
                 self._add(value, node.right)
             else:
                 node.right = Node(value) 
+
+    def search(self, value):
+        if (self.root != None):
+            return self._search(value, self.root)
+        else:
+            return None
+    
+    def _search(self, value, node):
+        if (value === node.data):
+            return node  
+        elif (value < node.data and node.left != None):
+            self._search(value, node.left)
+            print(value, True)
+        elif (value > node.data and node.right != None):
+            self._search(value, node.right)
+            print(value, True)
+
+    def preOrder(self):
+        if (self.root != None):
+            self._preOrder(self.root)
+
+    def _preOrder(self, node):
+        if (node != None):
+            print(node.data)
+            self._preOrder(node.left)
+            self._preOrder(node.right)
+    
+    # def remove(self, value):
+    #     if self.root is None:
+    #         return False
