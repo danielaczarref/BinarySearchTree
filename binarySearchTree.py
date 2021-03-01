c4311d33c753a527f25d8f566aa5e41377356122
Daniela Carvalho <daniela.czarref@gmail.com>
2021-03-01 20:45:40 -0300

    D - develoment of remove function for bst

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -55,6 +55,77 @@ class Tree:
             self._preOrder(node.left)
             self._preOrder(node.right)
     
-    # def remove(self, value):
-    #     if self.root is None:
-    #         return False
+    def remove(self, value):
+        if self.root is None:
+            return False
+
+        elif self.root.data == value:
+            if self.root.left is None and self.root.right is None:
+                self.root = None
+            elif self.root.left and self.root.right is None:
+                self.root = self.root.left
+            elif self.root.left is None and self.root.right:
+                self.root = self.root.right
+            elif self.root.left and self.root.right:
+                delParent = self.root
+                delNode = self.root.right
+                while delNode.left:
+                    delParent = delNode
+                    delNode = delNode.left
+
+                self.root.data = delNode.data
+                if delNode.right:
+                    if delParent.data > delNode.data:
+                        delParent.left = delNode.right
+                    elif delParent.data < delNode.data:
+                        delParent.right = delNode.right
+
+                else:
+                    if delNode.data < delParent.data:
+                        delParent.left = None
+                    else:
+                        delParent.right = None
+
+            return True
+
+        parent = None
+        newNode = self.root
+
+        while newNode and newNode.data != value:
+            parent = newNode
+            if value  < newNode.data:
+                newNode = newNode.left
+            elif value > newNode.data:
+                newNode = newNode.right
+
+        if newNode is None or newNode.data != value:
+            return False
+        elif newNode.left is None and newNode.right is None:
+            if value < parent.data:
+                parent.left = None
+            else:
+                parent.right = None 
+            return True
+        elif newNode.left is None and newNode.right:
+            if value < parent.data:
+                parent.left = newNode.right
+            else:
+                parent.right = newNode.right
+            return True
+        else:
+            delParent = newNode
+            delNode = newNode.right
+            while delNode.left:
+                delParent = delNode
+                delNode = delNode.left
+            newNode.data = delNode.data
+            if delNode.right:
+                if delParent.data > delNode.data:
+                    delParent.left = delNode.right
+                elif delParent.data < delNode.data:
+                    delParent.right = delNode.right
+            else:
+                if delNode.data < delParent.data:
+                    delParent.left = None
+                else:
+                    delParent.right = None
